ed645e0bfaeebbb39953dee292ea4723c82c8eab
Enbi81 <wotng2002@gmail.com>
2023-08-23 19:07:34 +0200

    Created base of the Introduction

--- src/app/front_page/components/introduction/FrontPageIntroduction.tsx
+++ src/app/front_page/components/introduction/FrontPageIntroduction.tsx
@@ -1,7 +1,28 @@
+import LucsonyMain from './assets/lucsony_main.jpg';
+
 export const FrontPageIntroduction = () => {
   return (
-    <div>
-      <div className={'text-4xl '}>City Corner Apartments</div>
+    <div
+      className={'min-h-screen flex items-center justify-center'}
+      style={{ backgroundImage: `url('${LucsonyMain}')` }}
+    >
+      <div className={'flex'}>
+        <div>
+          <div className={'text-6xl font-bold'}>City Corner Apartments</div>
+          <br />
+          <div>
+            Welcome to the elcome to the elcome to the elcome to the elcome to
+            the elcome to the elcome to the elcome to the elcome to the{' '}
+          </div>
+        </div>
+        <div>
+          <img
+            className={'bg-fixed'}
+            alt={'The living room of the apartment.'}
+            src={LucsonyMain}
+          />
+        </div>
+      </div>
     </div>
   );
 };
